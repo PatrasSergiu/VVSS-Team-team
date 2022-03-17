cdc14eb1fd3123878dc8e2e56590a36dacdea5b4
PatrasSergiu <patras.sergiu@yahoo.com>
2022-03-17 20:31:40 +0200

    Feature\Testlink

---
+++ 03_PizzaShop/src/main/java/pizzashop/controller/OrdersGUIControllerTest.java
@@ -0,0 +1,16 @@
+package pizzashop.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrdersGUIControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void ECP_ValidSum() {
+    }
+    @org.junit.jupiter.api.Test
+    void BVA_ValidSum() {
+    }
+    @org.junit.jupiter.api.Test
+    void BVA_InvalidSum() {
+    }
+}
---
+++ 03_PizzaShop/src/main/java/pizzashop/service/PaymentAlertTest.java
@@ -0,0 +1,12 @@
+package pizzashop.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentAlertTest {
+
+    @Test
+    void ECP_InvalidPaymentType() {
+    }
+}
